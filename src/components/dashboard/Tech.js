7925b88cddbf9ca70b96cb33f6da6cde07f40e65
rmit-s3510376-Harry-Utomo <s3510376@student.rmit.edu.au>
2017-10-15 00:54:30 +1100

    pullinh

--- src/components/dashboard/Tech.js
+++ src/components/dashboard/Tech.js
@@ -4,13 +4,36 @@ import firebase from 'firebase';
 import { Editor } from 'react-draft-wysiwyg';
 import { Table, Row, Col, Jumbotron, Button } from 'react-bootstrap';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import Modal from 'react-modal';
+
+const customStyles = {
+    overlay : {
+        position          : 'fixed',
+        top               : '0%',
+        left              : '0%',
+        right             : '0%',
+        bottom            : '0%',
+        backgroundColor   : 'rgba(255, 255, 255, 0.5)'
+    },
+    content : {
+        top                   : '10%',
+        left                  : '30%',
+        right                 : '30%',
+        bottom                : '10%',
+        marginRight           : '0%',
+        transform             : 'translate(0%, 0%)',
+        background            : 'rgba(255, 255, 255, 0)',
+        border                : '0px',
+    }
+};
 
 class Tech extends Component {
     state = {
         tickets: [],
         selectedTicket: null,
-        editorState: {},
+        editorState: null,
         priority:null,
+        modalIsOpen:false,
         status:"please specify status"
     }
 
@@ -43,6 +66,18 @@ class Tech extends Component {
             })
     }
 
+    afterOpenModal =()=> {
+        // references are now sync'd and can be accessed.
+        // this.subtitle.style.color = '#f00';
+    }
+
+    /* Close button for dialog */
+    closeModal =()=> {
+        this.setState({
+            modalIsOpen: false,
+            selectedTicket: null
+        });
+    }
 
     /* Update selectedTicket state */
     ticketUpdateClick = (ticket) => {
@@ -50,13 +85,6 @@ class Tech extends Component {
         this.setState({
             selectedTicket: (selectedTicket !== null && selectedTicket.id === ticket.id ? null : ticket)
         });
-    }
-
-    /* Close button for dialog */
-    closeDialogClick = () => {
-        this.setState({
-            selectedTicket: null
-        })
     }
 
     // Handle change on radio button on changing priority value
@@ -82,7 +110,7 @@ class Tech extends Component {
         var status = this.state.status;
         var comment = this.state.commentValue;
 
-        fetch(apiurl + '/api/inquiryCRUD'+ this.state.selectedTicket.id +'/update',
+        fetch(apiurl + "/api/inquiryCRUD" + this.state.selectedTicket.id +"/update",
             {
                 method: 'POST',
                 mode: 'CORS',
@@ -116,17 +144,14 @@ class Tech extends Component {
 
     /* render page */
     render () {
-        const vm = this;
-        const { tickets, selectedTicket, editorState } = this.state;
+        const vm = this
+        const { tickets, selectedTicket } = this.state
 
         return(
             <div>
                 <Row>
-                    <h1> My Tickets </h1>
-
-                    {/* if toggled, change the column layout */}
-
-                    <Col md={(selectedTicket !== null ? 7 : 12)}>
+                    <Col md={12}>
+                        <h1> My Tickets </h1>
                         {
                             // checking if tickets are assigned
                             tickets.length < 1 && (
@@ -155,7 +180,10 @@ class Tech extends Component {
                                         <td>{inquiry.status}</td>
                                         {/* each row has an update button and when a ticket is selected, update selectedTicket state */}
                                         <td>
-                                            <Button bsStyle={vm.state.selectedTicket !== null && vm.state.selectedTicket.id === inquiry.id ? 'success' : 'info'} onClick={() => vm.ticketUpdateClick(inquiry)}>Edit</Button>
+                                            <Button bsStyle={
+                                                    vm.state.selectedTicket !== null && vm.state.selectedTicket.id === inquiry.id ? 'success' : 'info'
+                                                } onClick={() => vm.ticketUpdateClick(inquiry)}>Edit
+                                            </Button>
                                         </td>
                                     </tr>
                                 ))
@@ -163,83 +191,87 @@ class Tech extends Component {
                             </tbody>
                         </Table>
                     </Col>
-                    {
-                        //if a ticket is selected, pop up a jumbotron dialog with specified layout
-                        selectedTicket !== null && (
-                            <Col md={5}>
-                                <Jumbotron style={{padding: 10}}>
-
-                                    {/* button to close dialog */}
-                                    <Button block bsStyle="success" onClick={this.closeDialogClick}>Close Dialog</Button>
-
-                                    {/* selectedTicket details */}
-                                    <h3 className="text-uppercase">Ticket Details</h3>
-                                    <p><b>ID: </b>{selectedTicket.id}</p>
-                                    <p><b>Title: </b><br/>{selectedTicket.title}</p>
-                                    <p><b>Comment: </b><br/>{selectedTicket.comment}</p>
-                                    <p><b>priority: </b><br/>{selectedTicket.comment}</p>
-                                    <p><b>level: </b><br/>{selectedTicket.comment}</p>
-
-                                    {
-                                        // render form to add comment to ticket
-                                        <form onSubmit={this.handleSubmit}>
-                                            <div class ="container">
-                                                <Editor
-                                                    editorState={editorState}
-                                                    toolbarOnFocus
-                                                    wrapperClassName="wrapper-class"
-                                                    editorClassName="editor-class"
-                                                    toolbarClassName="toolbar-class"
-                                                    onEditorStateChange={this.onEditorStateChange}
-                                                />
-                                            </div>
-
-                                                {/*  edit selectedTicket status  */}
-                                                <h3>Ticket Status</h3>
-                                                    <select value={this.state.status} onChange={this.handleStatusOptionChange} >
-                                                        <option value="resolved">Resolved</option>
-                                                        <option value="unresolved">Unresolved</option>
-                                                        <option value="pending">Pending</option>
-                                                        <option value="undefined">Undefined</option>
-                                                    </select>
-
-                                                {/* update selectedTicket priority through radio button */}
-                                                <h3>Ticket Priority</h3>
-                                            <div className="radio">
-                                                <label>
-                                                    <input type="radio" value="low"
-                                                           checked={this.state.priority === 'low'}
-                                                           onChange={this.handlePriorityOptionChange} />
-                                                    Low
-                                                </label>
-                                            </div>
-                                            <div className="radio">
-                                                <label>
-                                                    <input type="radio" value="medium"
-                                                           checked={this.state.priority === 'medium'}
-                                                           onChange={this.handlePriorityOptionChange} />
-                                                    Medium
-                                                </label>
-                                            </div>
-                                            <div className="radio">
-                                                <label>
-                                                    <input type="radio" value="high"
-                                                           checked={this.state.priority === 'high'}
-                                                           onChange={this.handlePriorityOptionChange} />
-                                                    High
-                                                </label>
-                                            </div>
-
-                                            <div className="clearfix"><br/>
-                                                <Button className="pull-right" typebsStyle="success" type="submit" value="Submit">Update</Button>
-                                            </div>
-                                        </form>
-                                    }
-                                </Jumbotron>
-                            </Col>
-                        )
-                    }
                 </Row>
+
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onAfterOpen={this.afterOpenModal}
+                    onRequestClose={this.closeModal}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                >
+                    {selectedTicket !== null && (
+                        <Jumbotron style={{padding: 10}}>
+
+                            {/* button to close dialog */}
+                            <Button block bsStyle="success" onClick={this.closeModal()}>close</Button>
+
+                            {/* selectedTicket details */}
+                            <h3 className="text-uppercase">Ticket Details</h3>
+                            <p><b>ID: </b>{selectedTicket.id}</p>
+                            <p><b>Title: </b><br/>{selectedTicket.title}</p>
+                            <p><b>Comment: </b><br/>{selectedTicket.comment}</p>
+                            <p><b>priority: </b><br/>{selectedTicket.comment}</p>
+                            <p><b>level: </b><br/>{selectedTicket.comment}</p>
+
+                            {
+                                // render form to add comment to ticket
+                                <form onSubmit={this.handleSubmit}>
+                                    <div class ="container">
+                                        <Editor
+                                            editorState={this.editorState}
+                                            toolbarOnFocus
+                                            toolbarClassName="toolbarClassName"
+                                            wrapperClassName="wrapperClassName"
+                                            editorClassName="editorClassName"
+                                            onEditorStateChange={this.onEditorStateChange}
+                                        />
+                                    </div>
+
+                                    {/*  edit selectedTicket status  */}
+                                    <h3>Ticket Status</h3>
+                                    <select value={this.state.status} onChange={this.handleStatusOptionChange} >
+                                        <option value="resolved">Resolved</option>
+                                        <option value="unresolved">Unresolved</option>
+                                        <option value="pending">Pending</option>
+                                        <option value="undefined">Undefined</option>
+                                    </select>
+
+                                    {/* update selectedTicket priority through radio button */}
+                                    <h3>Ticket Priority</h3>
+                                    <div className="radio">
+                                        <label>
+                                            <input type="radio" value="low"
+                                                   checked={this.state.priority === 'low'}
+                                                   onChange={this.handlePriorityOptionChange} />
+                                            Low
+                                        </label>
+                                    </div>
+                                    <div className="radio">
+                                        <label>
+                                            <input type="radio" value="medium"
+                                                   checked={this.state.priority === 'medium'}
+                                                   onChange={this.handlePriorityOptionChange} />
+                                            Medium
+                                        </label>
+                                    </div>
+                                    <div className="radio">
+                                        <label>
+                                            <input type="radio" value="high"
+                                                   checked={this.state.priority === 'high'}
+                                                   onChange={this.handlePriorityOptionChange} />
+                                            High
+                                        </label>
+                                    </div>
+
+                                    <div className="clearfix"><br/>
+                                        <Button className="pull-right" typebsStyle="success" type="submit" value="Submit">Update</Button>
+                                    </div>
+                                </form>
+                            }
+                        </Jumbotron>
+                    )}
+                </Modal>
             </div>
         );
     }
